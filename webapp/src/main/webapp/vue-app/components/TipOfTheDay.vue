ba925af47dc25eeac5109cef4c824e840a716417
Kateryna Nechyporchuk <kate@Katerynas-MacBook-Pro.local>
2020-08-17 14:43:55 +0300

    fixed getRandom tip method and add a getTipInfo, show&closeAddTipForm methods, add postTip method. Cleaned some code

--- webapp/src/main/webapp/vue-app/components/TipOfTheDay.vue
+++ webapp/src/main/webapp/vue-app/components/TipOfTheDay.vue
@@ -1,46 +1,98 @@
 <template>
   <v-app id="tipoftheday" class="VuetifyApp">
     <!-- TODO app markup here -->
-    <h1>{{ tip }}</h1>
+    <!-- <h1>{{ tipInfo.text }}</h1> -->
     <form>
       <i>?</i>
-      <textarea placeholder="Tip text..."></textarea>
-      <select>
-        <option value>--Please choose user--</option>
-        <option value="system">System</option>
-        <option value="viaUser">User</option>
-      </select>
-      <input type="date">
-      <button type="button" @click="getRandomTip">></button>
-      <button type="button">+</button>
-      <button type="button">x</button>
+      <textarea v-model="tipInfo.text" placeholder="Tip text..."></textarea>
+      <textarea v-show="show" v-model="tipInfo.poster" disabled></textarea>
+      <textarea v-show="show" v-model="tipInfo.posted" disabled></textarea>
+      <button type="button" @click="getTipInfo">></button>
+      <button type="button" @click="showAddTipForm">+</button>
+      <button type="button" @click="closeAddTipForm">x</button>
+    </form>
+    <form v-show="addTip" id="post" enctype="multipart/form-data" method="post" name="text">
+      <i>?</i>
+      <textarea v-model="tipInfo.text" placeholder="Tip text..."></textarea>
+      <button type="button" @click="postTip(tipInfo.text)">Save</button>
+      <button type="button" @click="closeAddTipForm">x</button>
     </form>
   </v-app>
 </template>
 
 <script>
 // TODO app scripts here
-import { getRandomTip } from "../utils";
+import { getRandomTip, postTip } from "../utils";
 
 export default {
   data() {
     return {
-      tip: "Hello world! Here will be tip of the day :)"
+      tipInfo: {
+        id: "",
+        posted: "$DATETIME",
+        poster: "via$USER",
+        text: "Hello world! Here will be tip of the day :)"
+      },
+      show: true,
+      addTip: false
     };
   },
   methods: {
-    getRandomTip
+    /**
+     * Rite a recived value to data 
+     * check if tip creator is "system"
+     * set a local date format
+     * @customFunction
+     */
+
+    getTipInfo: async function() {
+      const tipInfo = await getRandomTip();
+      this.show = tipInfo.poster !== "system" ? true : false;
+      tipInfo.posted = new Date(tipInfo.posted).toLocaleDateString();
+      this.tipInfo = tipInfo;
+    },
+    postTip,
+    showAddTipForm: function() {
+      this.addTip = true;
+      this.tipInfo.text = "";
+    },
+    closeAddTipForm: function() {
+      this.addTip = false;
+    }
   }
 };
 </script>
 
 <style>
 /* TODO app scripts here */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #tipoftheday {
   color: red;
 }
 form {
   display: flex;
   color: black;
+  align-items: center;
+}
+textarea {
+  resize: none !important;
+  width: 100%;
+  height: min-content;
+  background-color: rgb(168, 89, 89) !important;
+  padding: 0;
+  text-align: center;
+}
+textarea::placeholder {
+  /* background-color: white; */
+}
+button {
+  height: fit-content;
+}
+i {
+  height: fit-content;
 }
 </style>
--- webapp/src/main/webapp/vue-app/utils.js
+++ webapp/src/main/webapp/vue-app/utils.js
@@ -1,10 +1,34 @@
 const url = "/portal/rest/tipoftheday/";
+
+/**
+ * Fetch the random tip
+ *
+ * @customFunction
+ * @return Promise
+ */
+
 export async function getRandomTip() {
-  return await fetch(`${url}random/`, {
-    method: "GET",
-    contentType: "application/json",
-  })
+  return await fetch(`${url}random/`)
     .then((res) => res.json())
-    .then((text) => (this._data.tip = text.text))
     .catch((err) => console.log(err));
+}
+
+/**
+ * Post the custom tip
+ *
+ * @customFunction
+ * @return Promise
+ */
+
+export async function postTip(tip) {
+  const data = new URLSearchParams();
+  data.append("text", tip);
+  const response = await fetch(`${url}tip/`, {
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  return await response.json();
 }

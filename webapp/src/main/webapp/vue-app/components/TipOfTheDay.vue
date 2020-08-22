d89ba8547b1741f3c1a56faabccce724bc72f69b
Kateryna Nechyporchuk <kate@Katerynas-MacBook-Pro.local>
2020-08-22 17:06:00 +0300

    chaneged fade on textfield. Changed v-textarea to v-text-field. Fixed responsive. ixed tooltip isue

--- webapp/src/main/webapp/vue-app/components/TipOfTheDay.vue
+++ webapp/src/main/webapp/vue-app/components/TipOfTheDay.vue
@@ -3,7 +3,6 @@
     v-show="show" 
     id="tipoftheday" 
     class="VuetifyApp">
-    <h1>Tip of the day</h1>
 
     <!-- Another way to show/hide main tip pannel
     "{ display }" in this case defined inside data-->
@@ -11,46 +10,45 @@
     <v-form v-show="!addTip" class="form-tip form-tip-view">
       <i class="btn-tip">?</i>
       <v-tooltip 
-        bottom>
-        <template v-slot:activator="{ on, attr }">
-          <v-textarea
+        top>
+        <template 
+          v-slot:activator="{ on, attr }">
+          <v-text-field
             v-model="tipInfo.text"
             v-bind="attr"
             readonly="true"
-            rows="1"
-            wrap="off"
             single-line="true"
             dense="true"
-            outlined="true"
-            solo-inverted="true"
-            flat="true"
+            outlined
+            background-color="#f3f3f3"
             hide-details="true"
             placeholder="Tip text..."
+            class="custom-text-field"
             v-on="on"/>
         </template>
         <span class="tip-hint">{{ tipInfo.text }}</span>
       </v-tooltip>
-      <v-textarea
+      <v-text-field
         v-show="showTipData" 
-        v-model="tipInfo.poster" 
-        rows="1" 
+        v-model="tipInfo.poster"
+        background-color="#f3f3f3" 
         wrap="off" 
         dense="true"
-        outlined="true"
+        outlined
         hide-details="true"
-        solo-inverted="true" 
+        class="custom-text-field"
         readonly="true"/>
-      <v-textarea
+      <v-text-field
         v-show="showTipData" 
         v-model="tipInfo.posted" 
+        background-color="#f3f3f3"
         name="date" 
-        rows="1" 
         wrap="off" 
         dense="true"
         hide-details="true"
-        outlined="true"
-        solo-inverted="true" 
-        readonly="true"/>
+        outlined
+        class="custom-text-field"
+        readonly="true" />
       <div :class="{btnContainerShow: showTipData}" class="btn-container">
         <button 
           class="btn-tip" 
@@ -73,16 +71,17 @@
       method="post"
       name="text">
       <i class="btn-tip">?</i>
-      <v-textarea 
+      <v-textarea
         v-model="tipInfo.text" 
-        rows="2" 
-        wrap="off" 
-        placeholder="Tip text..."
-        dense="true"
-        hide-details="true"
+        background-color="#f3f3f3"
+        no-resize
         auto-grow
-        filled
-        solo-inverted="true"/>
+        rows="2"
+        dense
+        placeholder="Tip text..."
+        hide-details="true"
+        outlined
+        class="custom-text-field"/>
       <button 
         class="btn-tip" 
         type="button" 
@@ -172,68 +171,111 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #tipoftheday {
   box-sizing: border-box;
-  color: red;
 }
 .form-tip {
   display: flex;
   flex-flow: row wrap;
   color: black;
-  align-items: center;
   background-color: white;
-  border: 1px solid black;
+  border: 1px solid #b6b6b6;
   padding: 10px;
   min-width: 155px;
+
 }
-textarea {
-  resize: none !important;
-  height: 100% !important;
+ .custom-text-field.v-text-field--outlined >>> fieldset {
+  border-color: #b6b6b6 !important;
+  top: 0px;
+  padding-left: 0;
+}
+.custom-text-field.v-text-field--outlined >>> legend {
+  display: none;
+}
+.custom-text-field.v-text-field--outlined >>> input[type="text"] {
+  margin-bottom: 0;
   box-shadow: none;
-  overflow: hidden !important;
+  padding-left: 8px;
+  padding-right: 1px;
+  height: fit-content;
+}
+.custom-text-field.v-text-field--outlined >>> input[type="text"]::placeholder {
+  overflow: hidden;
+}
+.form-tip-view > .v-input:first-of-type >>> input[type="text"]:focus {
+  box-shadow: none;
+  border: 0px solid transparent !important;
+  outline: none;
+}
+.form-tip-view > .v-input:first-of-type >>> .v-text-field__slot::before:active {
+  box-shadow: none;
+   border: none !important;
+}
+.custom-text-field.v-text-field--outlined >>> .v-input__slot {
+    align-items: center;
+    min-height: fit-content;
+}
+.custom-text-field.v-text-field.v-text-field--enclosed >>> .v-input__slot {
+  padding: 0 0;
+}
+.custom-text-field.v-textarea >>> textarea {
+  resize: none;
+  box-shadow: none;
+  padding: 4px 0px 4px 8px !important;
+  padding-right: 0px;
+  margin-bottom: 0px;
+  border-radius: inherit;
+  height: 100%;
+  overflow: hidden;
   text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow-wrap: break-word;
- }
- textarea:focus {
-   box-shadow: none;
-   outline: none;
-   height: 100%;
- }
-.v-input__control {
-  margin: 0 8px 8px;
-}
-.form-tip-post > .v-input > .v-input__control > .v-input__slot > .v-text-field__slot > textarea {
   white-space: break-spaces;
+  overflow-wrap: anywhere;
+  max-width: calc(100% - 12px);
 }
-.form-tip-view > .v-input:nth-of-type(3) > .v-input__control > .v-input__slot > .v-text-field__slot {
-  flex: 1;
+.custom-text-field.v-text-field__slot {
+  margin-right: 0;
+  border-radius: inherit;
+}
+.form-tip-post > .v-input > .v-input__control > .v-input__slot > .v-text-field__slot > .v-text-field__slot {
+  white-space: break-spaces;
+  overflow-wrap: break-word;
+}
+.form-tip-view > .v-input:nth-of-type(3) {
+  flex: 2;
   min-width: 95px;
   max-width: fit-content;
+  margin: 0 4px 4px;
   }
-.form-tip-view > .v-input:nth-of-type(2) > .v-input__control > .v-input__slot > .v-text-field__slot {
+.form-tip-view > .v-input:nth-of-type(2) {
   flex: 1;
-  min-width: 95px;
+  min-width: 55px;
   max-width: fit-content;
+  margin: 0 4px 4px;
   }
-.form-tip-view > .v-input:first-of-type > .v-input__control > .v-input__slot > .v-text-field__slot {
+.form-tip-view > .v-input:first-of-type {
   flex: 6;
   min-width: 150px;
-  width: 100%;
+  max-width: 100%; 
+  margin: 0 4px 4px;
+}
+.form-tip-view > .v-input:first-of-type >>> .v-text-field__slot {
   overflow: hidden;
   text-overflow: ellipsis;
   content: "";
   position: relative;
+  border-radius: inherit;
 }
-.form-tip-view > .v-input:first-of-type > .v-input__control > .v-input__slot > .v-text-field__slot:before {
-  content: "";
-  width: 100%;
-  height: 100%;
+.form-tip-view > .v-input:first-of-type >>> .v-text-field__slot::before {
+  content: attr(tooltip);
+  width: calc(100% - 1px);
+  height: calc(100% - 2px);
   position: absolute;
   left: 0;
-  top: 0;
-  background: linear-gradient(to right, transparent 80%, white);
+  top: 1px;
+  background: linear-gradient(to right, transparent 80%, #f3f3f3);
+  border-radius: inherit;
+  pointer-events: none;
 } 
 .btn-container {
   display: inline-flex;
@@ -244,13 +286,56 @@ textarea {
   flex: 2;
 }
 .btn-tip {
-  margin: 0 3px;
-  padding: 0 3px;
+  margin: 0 4px 4px;
+  padding: 3px;
   height: 28px;
+  flex: 1;
   min-width: 28px;
-  border: 1px solid black !important;
+  max-width: fit-content;
+  border: 1px solid #b6b6b6 !important;
   border-radius: 5px;
   text-align: center;
   cursor: pointer;
+}
+@media only screen and (max-width: 530px) {
+  .form-tip {
+    display: grid;
+  }
+  .form-tip > i {
+    grid-row-start: 5;
+    justify-self: end;
+  }
+  .form-tip-view > .v-input:first-of-type  {
+    grid-column: 1 / span 2;
+  }
+  .form-tip-view > .v-input:nth-of-type(2) {
+    grid-row: 3;
+    grid-column: 1 / span 2;
+    max-width: 100%;
+    
+  }
+  .form-tip-view > .v-input:nth-of-type(3) {
+    grid-row: 4;
+    grid-column: 1 / span 2;
+    max-width: 100%;
+  }
+  .btn-container {
+    grid-row-start: 5;
+    margin: 0;
+  }
+  .form-tip-post > i{
+    grid-row: 2 / span 1;
+  }
+  .form-tip-post >>> .custom-text-field.v-textarea {
+    margin-bottom: 10px;
+    grid-column: 1 / span 3;
+  }
+  .form-tip-post > button:first-of-type {
+    grid-row: 2 / span 1;
+    max-width: 100%;
+  }
+  .form-tip-post > button:nth-of-type(2) {
+    grid-row: 2 / span 1;
+  }
 }
 </style>
